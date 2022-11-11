13c5e687aa4b7280e654e338b6037ae8fd218098
BogdanBlagojevicc <blagojevicbogdan1@gmail.com>
2022-11-11 10:30:55 +0100

    azuriranje sifre front

--- isafrontend/src/components/Careers.js
+++ isafrontend/src/components/Careers.js
@@ -1,98 +1,65 @@
-//import React, { useEffect, useState } from 'react'
-import React from "react";
-import Grid from "@mui/material/Grid";
-import { Button, Typography} from "@mui/material";
-import { green } from "@mui/material/colors";
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axiosApi from "../api/axios";
-import { useForm } from "react-hook-form";
-import InputTextField from "./InputTextField"; 
+import React, { useEffect, useState, useRef } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './styles/Contact.scss'
+import { Container } from '@mui/system';
+import {Paper, Button} from '@mui/material'
 
-const Careers = () => {
-  return (
-    <div>
-        <h1>I am a careers page</h1>
-    </div>
-  )
+export default function CenterAdministrator() {
+  const paperStyle = {padding: '50px 20px', width:600, margin:"20px auto"}
+  var [centerAdministrator, setCenterAdministrator] = useState([])
+  var [pas, setPas]  = useState('')
+
+  useEffect(() =>{
+    fetch("http://localhost:8081/api/centerAdministrators/getOne/1")
+    .then(res => res.json())
+    .then((result) =>
+    {
+      setCenterAdministrator(result);
+    }
+    )
+  },[])
+
+  const handleClick = (e) =>{
+    e.preventDefault()
+    var admin = centerAdministrator;
+    admin.password = pas;
+    console.log(admin);
+    fetch("http://localhost:8081/api/centerAdministrators/updatePassword/1",{
+    method:"PUT",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(admin)
+
+  }).then(() =>{
+    console.log("Admin changed")
+  })
 }
 
- /*function UpdateAdministratorCenterForm(){
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Container>
+        <h1>Show admin</h1>
+        <Paper elevation={3} style={paperStyle}>
 
- let navigate = useNavigate();
-  const {control, handleSubmit, reset} = useForm();
-  const [CenterAdministrator, setCenterAdministrator] = useState({});
+      
+        <TextField id="standard-basic" variant="standard" label="password" fullWidth 
+        placeholder={centerAdministrator.password}
+        onChange = {(e) => setPas(e.target.value)}
+        />
+  
+        <Button variant="contained" color="secondary" onClick={handleClick}>
+          Submit
+        </Button>
+        </Paper>
 
-  const handleUpdate = async (data) => {
-    try {
-      const resp = await axiosApi.put(`/api/centerAdministrators/updatePassword/${CenterAdministrator.id}/`, data);
-      console.log(resp.data);
-      navigate('/user-profile/');
-    }catch (error){
-      console.log(error.response);
-    }
-  }
-
-  const getAdministratorCenter = async (e) => {
-    try{
-      const res = await axiosApi.get('/api/centerAdministrators/user-profile/');
-      console.log("Administrator centra update", res.data);
-      setCenterAdministrator(res.data);
-      return res.data;
-    }catch (error){
-      console.log(error.response);
-    }
-  };
-
-  useEffect(() => {
-    getAdministratorCenter().then(reset);
-  },[]);
-
-  return(
-    <div>
-      <Typography variant="h4" color={green[800]} marginTop={2}>
-        Update profile
-      </Typography>
-      <Grid
-        container
-        marginTop={"-40px"}
-        rowspacing={2}
-        sx={{ padding: "55px 550px 0px 550px"}}
-      >
-        <Grid item xs={12}>
-          <InputTextField
-            name = "password"
-            control = {control}
-            variant = "filled"
-            label = "Password"
-            autoFocus
-            fullWidth
-          />
-      </Grid>
-        <Grid item xs = {12}>
-          <Button
-            type = "submit"
-            fullWidth
-            variant = "contained"
-            onClick={handleSubmit(handleUpdate)}
-            sx={{
-                mt: 3,
-                mb: 2,
-                background: "#6fbf73",
-                height: "30",
-                "&.MuiButtonBase-root":{
-                  "&:hover":{
-                    backgroundColor: green[600],
-                  },
-                },
-            }}
-          >
-            Submit
-          </Button>  
-        </Grid>
-      </Grid> 
-    </div>
+      </Container>
+    </Box>
   );
-}*/
-
-export default Careers
+}
